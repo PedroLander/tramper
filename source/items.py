cfeef780253238ce2cce9a474e14057968c5179c
pedrolander <pedro.alguez@gmail.com>
2024-11-10 01:08:43 +0100

    Loading items from a list

---
+++ source/items.py
@@ -0,0 +1,58 @@
+import pygame
+
+class Item():
+    def __init__(self, pos, scale):
+        self.pos = pos
+        self.scale = scale
+
+    def load_img(self, file):
+        img = pygame.image.load(file).convert_alpha()
+        img_width, img_height = img.get_size()
+        new_size = (int(img_width*self.scale),
+                    int(img_height*self.scale))
+        img = pygame.transform.scale(img, new_size)
+        return img
+
+class StaticItem(Item):
+    def __init__(self, pos, scale, img):
+        super().__init__(pos, scale)
+        self.img = super().load_img(img)
+
+class MovingItem(Item):
+    def __init__(self, pos, scale, f_img, b_img, l_img, r_img):
+        super().__init__(pos, scale)
+
+        self.f_img = super().load_img(f_img)
+        self.b_img = super().load_img(b_img)
+        self.l_img = super().load_img(l_img)
+        self.r_img = super().load_img(r_img)
+
+        self.set_facing("front")
+
+    def set_facing(self, facing):
+        if facing == "front":
+            self.img = self.f_img
+        if facing == "back":
+            self.img = self.b_img
+        if facing == "left":
+            self.img = self.l_img
+        if facing == "right":
+            self.img = self.r_img
+
+    def move(self, delta):
+        self.pos = [a+b for a, b in zip(self.pos, delta)]    
+
+class Human(MovingItem):
+    def __init__(self, pos):
+        front = "images/icons/sheriff_front.png"
+        back = "images/icons/sheriff_back.png"
+        left = "images/icons/sheriff_left.png"
+        right = "images/icons/sheriff_right.png"
+        scale = 6
+        super().__init__(pos, scale, front, back, left, right)
+
+class Rock(StaticItem):
+    def __init__(self, pos):
+        front = "images/icons/rock1.png"
+        scale = 2
+        super().__init__(pos, scale, front)
--- source/tramper.py
+++ source/tramper.py
@@ -1,21 +1,18 @@
 import pygame
+from items import Human, Rock
 
 class Game():
     def __init__(self):
         self.screen_width = 500
         self.screen_height = 400
 
-        pelayo = {"kind":"sheriff", "views":True, "pos":[0,0],"scale":6,
-                "view":None, rect:None}
-        rock1 = {"kind":"rock","views":False, "pos":[-100,-100],"scale":3,
-                "view":None, rect:None}
-        rock2 = {"kind":"rock","views":False, "pos":[-150,200],"scale":3,
-                "view":None, rect:None}
-        items = [pelayo, rock1, rock2]
-    
         pygame.init()
         screen = pygame.display.set_mode(
                 (self.screen_width,self.screen_height))
+
+        h1 = Human([0,0])
+        r1 = Rock((100,100))
+        items = [h1, r1]
 
         background = pygame.Surface(
                 (self.screen_width,self.screen_height))
@@ -26,32 +23,6 @@ class Game():
         def pos_to_pix(pos):
         #transform possition to pixels
             return (pos[0]+(self.screen_width//2),pos[1]+self.screen_height//2)
-
-        for item in items:
-
-            if item["views"]==True:
-            #load sheriff views
-                s_views = {}
-                for view in ["front","back","left","right"]:
-                    image = pygame.image.load("images/icons/{0}_{1}.png"
-                            .format(item["kind"],view))
-                    image_width, image_height = image.get_size()
-                    new_size = (int(image_width*item["scale"]),
-                        int(image_height*item["scale"]))  
-                    image = pygame.transform.scale(image, new_size)
-                    s_views[view] = image.convert_alpha()
-            elif item["views"]==False:
-                image = pygame.image.load("images/icons/{0}1.png"
-                        .format(item["kind"]))
-                image_width, image_height = image.get_size()
-                new_size = (int(image_width*item["scale"]),
-                    int(image_height*item["scale"]))
-                image = pygame.transform.scale(image, new_size)
-                item["rect"] = image.get_rect()
-
-
-
-        facing = "front"
 
         running = True
     
@@ -65,22 +36,21 @@ class Game():
             keys = pygame.key.get_pressed()
         
             if keys[pygame.K_DOWN]:
-                facing = "front"
-                items[0]["pos"][1]+=.2
+                h1.set_facing("front")
+                h1.move((0,0.2))
             if keys[pygame.K_UP]:
-                facing = "back"
-                items[0]["pos"][1]-=.2
+                h1.set_facing("back")
+                h1.move((0,-0.2))
             if keys[pygame.K_LEFT]:
-                facing = "left"
-                items[0]["pos"][0]-=.2
+                h1.set_facing("left")
+                h1.move((-0.2,0))
             if keys[pygame.K_RIGHT]:
-                facing = "right"
-                items[0]["pos"][0]+=.2
+                h1.set_facing("right")
+                h1.move((0.2,0))
 
-            image_rect = s_views[facing].get_rect()
-            image_rect.center = pos_to_pix(items[0]["pos"])
-            screen.blits(blit_sequence=(
-                (item["rect"] for item in items)))
+            #image_rect = s_views[facing].get_rect()
+            #image_rect.center = pos_to_pix(items[0]["pos"])
+            screen.blits([(item.img,pos_to_pix(item.pos)) for item in items])
 
             pygame.display.flip()
 
